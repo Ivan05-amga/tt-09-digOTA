788a1017a2a62185bbbc24fc81080d9ae3ec8973
Jorge Ivan <82187411+Ivan05-amga@users.noreply.github.com>
2024-11-07 12:07:34 -0300

    Create digOTA.v

---
+++ src/digOTA.v
@@ -0,0 +1,20 @@
+module digOta (
+    input Vip, 
+    input Vin, 
+    output Out
+);
+    wire INn, INp, Op, On, CM , En;
+
+
+    assign INn = ~Vip; // IV1 
+    assign INn = ~CM;  // IV2
+    assign INp = ~Vin; //IV3 
+    assign INp = ~CM;  //IV4 
+    assign Op = ~INn; // IV5
+    assign On = ~INp; //IV6 
+    assign En = Op ^ On; // XOR1 
+    assign Out = En ? Op : 1'bZ; //BT1
+    assign CM = ~En ? ~Op : 1'bZ;
+
+
+endmodule
