15210f1c76c985544159884aabd53e6c864942ca
Jorge Ivan <82187411+Ivan05-amga@users.noreply.github.com>
2024-11-07 13:36:00 -0300

    Update digOta.v

--- src/digOta.v
+++ src/digOta.v
@@ -13,8 +13,8 @@ module digOta (
     assign Op = ~INn; // IV5
     assign On = ~INp; //IV6 
     assign En = Op ^ On; // XOR1 
-    assign Out = En ? Op : 1'bZ; //BT1
-    assign CM = ~En ? ~Op : 1'bZ;
+    assign Out = En ? Op : 1'b0; //BT1
+    assign CM = ~En ? ~Op : 1'b0;
 
 
 endmodule
