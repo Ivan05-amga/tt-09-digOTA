1006a2d2957312a3750a706400ff811f8c4a741b
Jorge Ivan <82187411+Ivan05-amga@users.noreply.github.com>
2024-11-07 12:10:14 -0300

    Update project.v

--- src/project.v
+++ src/project.v
@@ -24,4 +24,10 @@ module tt_um_example (
   // List all unused inputs to prevent warnings
   wire _unused = &{ena, clk, rst_n, 1'b0};
 
+  digOta digOta_isnt(
+      .Vip(ui_in[0]),
+      .Vin(ui_in[1]),
+      .Out(uo_out[0])
+  );
+
 endmodule
