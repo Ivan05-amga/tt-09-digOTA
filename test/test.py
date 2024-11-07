c0ba00cbdaf2a916b5dd06e8f805865e206a8d60
Jorge Ivan <82187411+Ivan05-amga@users.noreply.github.com>
2024-11-07 14:22:15 -0300

    Update test.py

--- test/test.py
+++ test/test.py
@@ -33,7 +33,7 @@ async def test_project(dut):
     dut.ui_in[1].value = 0
     await ClockCycles(dut.clk, 1)
     dut._log.info(f"Value of uo_out[0]: {dut.uo_out[0].value}")
-    assert dut.uo_out[0].value == 1
+    assert dut.uo_out[0].value == BinaryValue("Z")
 
 
     
